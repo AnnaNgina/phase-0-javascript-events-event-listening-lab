b2a21b333140bba5b01cb7c6fbe92d88a5539c40
annastacia <annastaciangina96@gmail.com>
2022-06-14 17:35:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    alert('i have been clicked')
+
+input.addEventListener('click',function(){
+
+});
+    
 }
